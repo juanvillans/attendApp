fe122cfc126dbabf61251b367564cedf2522a776
Juan Francisco Villasmil Tovar <juanvillans16@gmail.com>
2023-12-25 05:02:11 -0400

    asistencia is the index

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const path = require("path")
 const Users = require("../models/Users") 
 
-router.get("/asistencias", async (req, res) => {
+router.get("/", async (req, res) => {
     try {
         res.sendFile(path.join(__dirname, "../public/", "asistencia.html"))
 
